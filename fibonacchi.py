f68f705e99c68c4c96c990c1f534ec7dcd936baa
AlinaProkhorovich <prokhorovichalina@gmail.com>
2022-02-07 17:14:02 +0300

    Changed name func

--- fibonacchi.py
+++ fibonacchi.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
+def fib(n):
     if n == 1 or n == 2:
         return 1
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return fib(n - 1) + fib(n - 2)
 
 
-print(fibonacci(int(input("Which element of the Fibonacci series do you want to know? "))))
+print(fib(int(input("Which element of the Fibonacci series do you want to know? "))))
